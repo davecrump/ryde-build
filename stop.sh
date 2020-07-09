8074ce97c16976657fa731924feacbca473d0123
Dave Crump <dave.g8gkq@gmail.com>
2020-07-09 17:00:33 +0100

    202007080 Add new Remote Config File

--- stop.sh
+++ stop.sh
@@ -2,6 +2,12 @@
 
 # File to stop Ryde
 
-sudo killall python3
+sudo killall python3 >/dev/null 2>/dev/null
+
+sleep 1
+if pgrep -x "python3" >/dev/null
+then
+  sudo killall -9 python3 >/dev/null 2>/dev/null
+fi
 
 exit
